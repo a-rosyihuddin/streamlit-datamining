966fee1cd41a3d2e0fcd808c08011378524daa3e
a-rosyihuddin <arosyihuddin6@gmail.com>
2022-11-26 06:25:01 +0700

    Update

--- Home.py
+++ Home.py
@@ -5,14 +5,19 @@ from streamlit_extras.app_logo import add_logo
 add_logo("http://placekitten.com/150/150")
 st.snow()
 st.markdown("# Home")
+st.text("""
+         Nama   : Ahmad Rosyihuddin
+         NIM    : 200411100126
+         Kelas  : Data Mining A
+         """)
 st.write("""
-         Nama   : Ahmad Rosyihuddin\n
-         NIM    : 200411100126\n
-         Kelas  : Data Mining A\n
+         Email  : arosyihuddin6@gmail.com\n
+         Github : [Github Repositori](https://github.com/a-rosyihuddin/streamlit-datamining)
          """)
 
-st.title("Dataset")
-st.write('Dataset yang di gunakan merupakan Historis data dari Bitcoin dari tanggal 1 Februari 2018 sampai dengan 30 Oktober 2022')
+st.markdown("# Apa Itu Data Mining")
+st.caption("Data mining adalah proses pengumpulan dan pengolahan data yang bertujuan untuk mengekstrak informasi penting pada data. Proses pengumpulan dan ekstraksi informasi tersebut dapat dilakukan menggunakan perangkat lunak dengan bantuan perhitungan statistika, matematika, ataupun teknologi Artificial Intelligence (AI). Data mining sering disebut juga Knowledge Discovery in Database (KDD).")
+
 
 # Load Dataset
 
