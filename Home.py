2e04688d0af56a12b90060100c3fed6eafe2db75
a-rosyihuddin <arosyihuddin6@gmail.com>
2022-11-28 09:06:11 +0700

    update

--- Home.py
+++ Home.py
@@ -17,12 +17,12 @@ st.write("""
 
 st.markdown("# Klasisfikasi Range Harga Berdasarkan Spesifikasi Handphone")
 st.text(""" Data yang di gunakan untuk klasifikasi range harga memiliki 17 fitur, 
-        dan dari 17 fitur ini sudah termasuk dengan label, 
-        yang value nya berupa index dari 0-3 dengan penjelasan sebagai berikut:
-        > 0 - Low Cost
-        > 1 - Medium Cost
-        > 2 - High Cost
-        > 3 - Very High Cost """)
+dan dari 17 fitur ini sudah termasuk dengan label, yang value nya berupa
+index dari 0-3 dengan penjelasan sebagai berikut:
+  > 0 - Low Cost
+  > 1 - Medium Cost
+  > 2 - High Cost
+  > 3 - Very High Cost """)
 
 
 # Load Dataset
